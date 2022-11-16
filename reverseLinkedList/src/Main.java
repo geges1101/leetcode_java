a911b7adbc5733f4786de4fe5b8f3e27f650d4db
geges1101 <62469977+geges1101@users.noreply.github.com>
2022-11-16 23:04:52 +0300

    Add files via upload

---
+++ reverseLinkedList/src/Main.java
@@ -0,0 +1,12 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode dummy = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = dummy;
+            dummy = head;
+            head = next;
+        }
+        return dummy;
+    }
+}
