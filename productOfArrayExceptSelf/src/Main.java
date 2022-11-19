983df01078560953e6de5cdb1c41fccb3bb268e3
geges1101 <62469977+geges1101@users.noreply.github.com>
2022-11-19 22:55:50 +0300

    Add files via upload

---
+++ productOfArrayExceptSelf/src/Main.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        int curr = 1;
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = curr;
+            curr *= nums[i];
+        }
+        curr = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] *= curr;
+            curr *= nums[i];
+        }
+        return result;
+    }
+}
